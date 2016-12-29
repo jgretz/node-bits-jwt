a576e0f80b8bf64ab8ffce5ca2a2ca1d9f342643
jgretz <jgretz@truefitsolutions.com>
2016-12-29 14:01:15 -0500

    upgrade dependencies, use constants fron node-bits

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-export default () => {};
+const DEFAULT_CONFIG = {
+  authorize: 'api/authorize',
+};
+
+export default (config) => {
+  const options = Object.assign(DEFAULT_CONFIG, config);
+
+  return {
+    beforeConfigureRoutes: (args) => {
+    },
+
+    afterConfigureRoutes: (args) => {
+    }
+  };
+};
