6105f31feaa66e90cb6fd06908a53228f3f1cf7a
Nick <nstewart@truefit.io>
2017-02-06 15:58:53 -0500

    Correct HTTP status code

--- src/util/authorize.js
+++ src/util/authorize.js
@@ -67,6 +67,6 @@ export const authorize = (config, database) => (req, res) => {
     })
     .catch((err) => {
       logError(err);
-      res.json({ success: false });
+      res.status(400).json({ success: false });
     });
 };
